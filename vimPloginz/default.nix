34a12bca345987e7fb61eb9c018a4499a1d7bcda
li <@li:maisiliym.uniks>
2021-03-09 11:00:51 +0000

    +expressline

--- vimPloginz/default.nix
+++ vimPloginz/default.nix
@@ -5,11 +5,11 @@ let
 
   implaidSpoks = {
     inherit (hob) astronauta-nvim barbar-nvim bufferize-vim BufOnly-nvim
-      completion-nvim completion-buffers dwm-vim formatter-nvim FTerm-nvim
-      fzf-lsp-nvim fzf-vim galaxyline-nvim gina-vim gitsigns-nvim hop-nvim
-      kommentary lspsaga-nvim lsp-status-nvim nvim-autopairs nvim-base16-lua
-      nvim-bufferline-lua nvim-colorizer-lua nvim-fzf nvim-fzf-commands
-      nvim-lazygit nvim-lspconfig nvim-lspfuzzy nvim-treesitter
+      completion-nvim completion-buffers dwm-vim express_line-nvim formatter-nvim
+      FTerm-nvim fzf-lsp-nvim fzf-vim galaxyline-nvim gina-vim gitsigns-nvim
+      hop-nvim kommentary lspsaga-nvim lsp-status-nvim nvim-autopairs
+      nvim-base16-lua nvim-bufferline-lua nvim-colorizer-lua nvim-fzf
+      nvim-fzf-commands nvim-lazygit nvim-lspconfig nvim-lspfuzzy nvim-treesitter
       nvim-treesitter-context nvim-treesitter-refactor
       nvim-treesitter-textobjects nvim-tree-lua nvim-web-devicons lir-nvim neogit
       plenary-nvim popup-nvim ron-vim telescope-nvim tokei-vim vim-visual-multi
